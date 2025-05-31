2a3f06aa98cff92f671bd7ff02db78b85a271658
Cjay13 <herath.charithjay@gmail.com>
2025-05-31 19:41:43 +1000

    added vnet configuration

---
+++ resource_group.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "user-management" {
+    name = "user-management-infra"
+    location = "Australia East"
+  
+}
---
+++ vnet.tf
@@ -0,0 +1,30 @@
+resource "azurerm_network_security_group" "user-management-nsg" {
+  name                = "user-management-nsg"
+  location            = azurerm_resource_group.user-management.location
+  resource_group_name = azurerm_resource_group.user-management.name
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "user-management-vnet"
+  location            = azurerm_resource_group.user-management.location
+  resource_group_name = azurerm_resource_group.user-management.name
+  address_space       = ["10.1.0.0/26"]
+
+
+  subnet {
+    name             = "subnet1"
+    address_prefixes = ["10.1.0.0/27"]
+    security_group   = azurerm_network_security_group.user-management-nsg.id
+  }
+
+  subnet {
+    name             = "subnet2"
+    address_prefixes = ["10.1.0.32/27"]
+    security_group   = azurerm_network_security_group.user-management-nsg.id
+  }
+
+
+  tags = {
+    project = "user-management"
+  }
+}
