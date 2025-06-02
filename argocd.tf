a402874a7ec3fc7088e655de9c04bc78c3091e00
Cjay13 <herath.charithjay@gmail.com>
2025-06-02 18:12:44 +1000

    removed argocd config

--- argocd.tf
+++ argocd.tf
@@ -1,8 +1,8 @@
-module "argocd" {
-    source = "git::https://github.com/Cjay13/terraform-k8s-argocd.git?ref=main"
-    namespace = var.argocd_namespace
-    enable_ingress = var.argocd_enable_ingress
-    ingressClassName = var.ingress_class_name
-    enable_tls = var.argocd_enable_tls
-    domainName = var.argocd_domainName
-}
+# module "argocd" {
+#     source = "git::https://github.com/Cjay13/terraform-k8s-argocd.git?ref=main"
+#     namespace = var.argocd_namespace
+#     enable_ingress = var.argocd_enable_ingress
+#     ingressClassName = var.ingress_class_name
+#     enable_tls = var.argocd_enable_tls
+#     domainName = var.argocd_domainName
+# }
